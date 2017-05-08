42d0df218fbba44ef297b79d00578ff2471a3cd1
gtdean11 <gtdean11@gmail.com>
2017-05-08 11:38:08 -0500

    Created first package
    Followed steps on
    https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
    to create a package

---
+++ creating_packages.r
@@ -0,0 +1,10 @@
+#these are required to create packages
+
+#install packages
+install.packages("devtools")
+devtools::install_github("klutometis/roxygen")
+
+
+#include packages
+library("devtools")
+library("roxygen2")
