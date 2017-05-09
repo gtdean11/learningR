2ffa86606c907c8fc6dc91eb9abeea9f331ae7eb
gtdean11 <gtdean11@gmail.com>
2017-05-08 23:35:22 -0500

    Added R file that documents how to create a package

---
+++ creating-a-package.R
@@ -0,0 +1,23 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+#
+#
+#
+# In RStudio, click new project, new directory, R package
+# Name package, enter location
+# Put R files in the R folder
+# Click Build package (may require devtools package)
+# Check package
+# Add to github and commit
+# On destination computer, click install_github("username/repository")  , which may require devtools
+# you will have to use library("pkgname") to run functions
+# package can be removed with remove.packages("packagename")
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
